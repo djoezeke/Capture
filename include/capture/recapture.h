55621a1cc27433317241021db04c8ef0786afc46
Sackey Ezekiel Etrue <sackeyetrue@gmail.com>
2026-06-09 14:32:16 +0000

    Utility Macros

--- include/capture/recapture.h
+++ include/capture/recapture.h
@@ -1,3 +1,35 @@
 /**
  * @file capture/recapture.h
  */
+
+#ifndef DJOEZEKE_UNCAPTURE_H
+#define DJOEZEKE_UNCAPTURE_H
+
+//-----------------------------------------------------------------------------
+// [SECTION] Configurations
+//-----------------------------------------------------------------------------
+
+/**
+ * @defgroup configuration Library Configurations.
+ * @brief Preprocessor macros for configuring library functionality.
+ * @{
+ */
+
+/**
+ * @brief Configure file with user config.
+ */
+#ifdef CAPTURE_CONFIG
+#include CAPTURE_CONFIG
+#endif // CAPTURE_CONFIG
+
+/** @} configuration */
+
+//-----------------------------------------------------------------------------
+// [SECTION] Capture Macros
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// [SECTION] User's Macros
+//-----------------------------------------------------------------------------
+
+#endif // DJOEZEKE_UNCAPTURE_H
