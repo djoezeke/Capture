507197eab435bc6594feb0a77bde29f1f8315738
Sackey Ezekiel Etrue <sackeyetrue@gmail.com>
2026-06-20 06:15:03 +0000

    extended macros

--- include/capture/uncapture.h
+++ include/capture/uncapture.h
@@ -9,8 +9,30 @@
 // [SECTION] Capture General Cleanup
 //-----------------------------------------------------------------------------
 
+#if 1 // def MACRO
+
+#undef CAPTURE_UNUSED
+#undef CAPTURE_RESTRICT
+#undef CAPTURE_HAS_BUILTIN
+#undef CAPTURE_DEPRECATED
+#undef CAPTURE_UNUSED
+#undef CAPTURE_INLINE
+#undef CAPTURE_FORCE_INLINE
+#undef CAPTURE_NODISCARD
+#undef CAPTURE_NORETURN
+#undef CAPTURE_FALLTHROUGH
+#undef CAPTURE_ALLOC_SIZE
+#undef CAPTURE_ALLOC_SIZE2
+#undef CAPTURE_ALIGNED
+
+#endif // MACRO
+
 //-----------------------------------------------------------------------------
 // [SECTION] User's General Cleanups
 //-----------------------------------------------------------------------------
+
+#pragma region User
+
+#pragma endregion // User
 
 #endif // DJOEZEKE_RECAPTURE_H
